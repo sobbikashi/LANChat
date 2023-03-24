636489abf66e44426ecaaeb68924c10e0181ffbf
Vincent Valentine <vanilmirth@inbox.ru>
2023-03-24 14:31:19 +0300

    Добавьте файлы проекта.

---
+++ LANChat/IServiceChat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LANChat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract =typeof(IServerChatCallback))]  //здесь мы передаём название интерфейса, который будет реализовывать CallbackContract
+    public interface IServiceChat
+    {
+        [OperationContract] //здесь атрибута IsOneWay нет, потому что нужно обязательно дождаться, когда команда отработает и даст клиенту ID
+        int Connect(string name);
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)] // этот атрибут показывает, что не нужно ждать ответа от сервера об успешном выполнении работы метода
+        void SendMsg(string msg, int id);       
+       
+    }
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay =true)]
+        void MsgCallback(string msg);
+    }
+}
---
+++ LANChat/ServiceChat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LANChat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)] //Три варианта создания сервиса: по вызову, по сессии или сингл, то есть единый для всех
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>(); //список юзеров чата
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser() //создаём нового юзера
+            {
+                ID = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+            SendMsg(user.Name + " вошёл в чат", 0);
+            users.Add(user); //добавляем его в список
+            return user.ID; //отдаём клиенту его Id
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(i => i.ID == id);
+            if (user != null)
+            {
+                users.Remove(user);
+                SendMsg(user.Name + " покинула чат ©", 0);
+            }
+        }
+
+        public void SendMsg(string msg, int id)
+        {
+            foreach (var item in users)
+            {
+                string answer = DateTime.Now.ToShortTimeString(); //формируем сообщение: дата, имя юзера (если это коннект или дисконнект, ид юзера равен 0, соответственно, он
+                var user = users.FirstOrDefault(i => i.ID == id); // не ищется, затем собственно текст сообщения
+                if (user != null)
+                {
+                    answer += ":  " + user.Name + " ";
+                }
+                answer += msg;
+
+                item.operationContext.GetCallbackChannel<IServerChatCallback>().MsgCallback(answer); //вызываем кооллбэк у клиента, передаём ему наше сообщение
+
+            }
+
+        }
+    }
+}
+
